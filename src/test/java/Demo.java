d8f0d9ae116eeafec201658dfec71c5ab0cba10c
Yavuz <yavuzgecikli@hotmail.com>
2021-06-03 14:27:29 -0400

    first commit from local yavuz branch

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+        System.out.println("yavuz branch is just created");
     }
 }
