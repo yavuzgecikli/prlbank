4958b67fcb19ede8ee216228f8299729674970a3
yavuzgecikli <62161813+yavuzgecikli@users.noreply.github.com>
2021-06-03 16:55:55 -0400

    Update Demo.java
    Hi has been added.

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
         System.out.println("yavuz branch is just created");
+        //Hi
     }
 }
